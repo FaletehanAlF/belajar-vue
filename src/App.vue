2f7929d71d3836b4b7c5e6a6c603f51d0e7bd35a
FaletehanAlF <faletehanalfarabi09>
2026-09-02 21:57:33 +0700

    feat: learn ref reactive state

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,21 @@
 <script setup lang="ts">
-import Dashboard from "./views/Dashboard.vue";
+import { ref } from 'vue'
+
+const count = ref(0)
+
+const tambah = () => {
+  count.value++
+}
 </script>
 
 <template>
-  <Dashboard />
+  <div>
+    <h1>Belajar ref()</h1>
+
+    <p>Nilai: {{ count }}</p>
+
+    <button @click="tambah">
+      Tambah
+    </button>
+  </div>
 </template>
