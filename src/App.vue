227fc19dbfeabc8e3d10ba49ce4dc72ba34b2ceb
FaletehanAlF <faletehanalfarabi09>
2026-09-02 22:09:57 +0700

    feat: learn list rendering

--- src/App.vue
+++ src/App.vue
@@ -1,37 +1,23 @@
 <script setup lang="ts">
-import { ref } from 'vue'
-
-const isLoggedIn = ref(false)
-
-const login = () => {
-  isLoggedIn.value = true
-}
-
-const logout = () => {
-  isLoggedIn.value = false
-}
+const fruits = [
+  'Apel',
+  'Pisang',
+  'Jeruk',
+  'Mangga',
+  'Anggur'
+]
 </script>
 
 <template>
   <div>
-    <h1>Belajar v-if & v-else</h1>
+    <h1>Belajar v-for</h1>
 
-    <div v-if="isLoggedIn">
-      <h2>Selamat datang! 👋</h2>
-      <p>Kamu sudah login.</p>
+    <h2>Daftar Buah</h2>
 
-      <button @click="logout">
-        Logout
-      </button>
-    </div>
-
-    <div v-else>
-      <h2>Silakan login</h2>
-      <p>Kamu belum login.</p>
-
-      <button @click="login">
-        Login
-      </button>
-    </div>
+    <ul>
+      <li v-for="fruit in fruits" :key="fruit">
+        {{ fruit }}
+      </li>
+    </ul>
   </div>
 </template>
